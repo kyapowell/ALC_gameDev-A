2c37f5a865f47d02fd4a7b4af6e9207244919642
Kya <kyampowell@gmail.com>
2020-09-17 12:39:17 -0600

    Connected to Sim

---
+++ Prototype 1 - Driving Sim/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,5,-7);
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes camera match players movement / position
+        transform.position = player.transform.position + offset;
+    }
+}
