f3c1923aac8d14af45bbc1d894a7cbb49d3e5779
Kya <kyampowell@gmail.com>
2020-09-29 12:36:22 -0600

    added challenge 1

---
+++ Challenge 1 - Plane Programming/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+        public float propellerSpeed = 1000;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate propeller
+        transform.Rotate(Vector3.forward * Time.deltaTime * propellerSpeed);
+    }
+}
