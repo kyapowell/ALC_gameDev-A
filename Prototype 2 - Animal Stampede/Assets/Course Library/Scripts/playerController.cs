029cad68ffcc349270c4654ae9c0087d95392527
Kya <kyampowell@gmail.com>
2020-10-05 12:41:40 -0600

    prototype 2
    created prototype 2 project plus day 1 code

---
+++ Prototype 2 - Animal Stampede/Assets/Course Library/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    //constrains players movement to the left at -10
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+    //contrains players movement to the right at 10
+        if(transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+    }
+}
