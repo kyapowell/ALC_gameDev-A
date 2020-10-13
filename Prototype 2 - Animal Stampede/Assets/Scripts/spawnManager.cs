48a1a88b64820ee69158103e22406ac22dd3af66
Kya <kyampowell@gmail.com>
2020-10-13 12:40:01 -0600

    added spawning
    animal spawn function- there are errors still

---
+++ Prototype 2 - Animal Stampede/Assets/Scripts/spawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    //public int animalIndex;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
