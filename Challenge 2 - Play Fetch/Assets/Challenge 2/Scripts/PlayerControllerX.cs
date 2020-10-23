5e292643ad7bc20cba377a3b085f4b8d945afa8d
Kya <kyampowell@gmail.com>
2020-10-23 12:39:27 -0600

    added changing balls and intervals
    still need to fix dog spawn

--- Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float fireRate = 1; //time the player has to wait to fire again
+    private float nextFire = 0; //time since start tl player can fire again
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+      //reset nextFire to current time plus fire rate      
+            nextFire = Time.time + fireRate; 
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
