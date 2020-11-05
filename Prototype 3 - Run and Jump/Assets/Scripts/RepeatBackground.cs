38690b88f3f6c28b85f2f46d9d66a41bc058ffe6
Kya <kyampowell@gmail.com>
2020-11-05 12:44:12 -0700

    prefabs and more scripts
    haha

---
+++ Prototype 3 - Run and Jump/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the background is less than -50 on x, reset position
+        if(transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
