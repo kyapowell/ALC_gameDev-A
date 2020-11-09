27d817ed7bedb3b9737f8fc8ad1c33cc269606b9
Kya <kyampowell@gmail.com>
2020-11-09 12:40:14 -0700

    Added gameOver
    added gameOver and links to other scripts

--- Prototype 3 - Run and Jump/Assets/Scripts/SpawnManager.cs
+++ Prototype 3 - Run and Jump/Assets/Scripts/SpawnManager.cs
@@ -9,20 +9,21 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatDelay = 2;
     
+    private PlayerController playerControllerScript;
+    
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 
 }
