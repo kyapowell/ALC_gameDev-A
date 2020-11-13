b8544988cdd6b82323628445605a03a2e70c158b
Kya <kyampowell@gmail.com>
2020-11-13 12:38:35 -0700

    Added jumping animation
    cant jump when dead

--- Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
@@ -10,22 +10,29 @@ public class PlayerController : MonoBehaviour
     
     public bool isOnGround = true;
     public bool gameOver = false;
+
+    private Animator playerAnim;
+
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
+        
     }
     
     private void OnCollisionEnter(Collision collision)
@@ -38,6 +45,9 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
         }
     
         
