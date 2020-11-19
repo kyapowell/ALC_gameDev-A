82a91880845bf608f6a988a67e60f6fb67c982e6
Kya <kyampowell@gmail.com>
2020-11-19 12:12:22 -0700

    Finished prototype 3
    added sound effects

--- Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,8 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityModifier;
 
         playerAnim = GetComponent<Animator>();
+
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,6 +39,7 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
         
     }
@@ -52,6 +59,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     
         
