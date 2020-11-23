bbc8c2cfec8ff6a2b1bc1b2380305ad72af721e0
Kya <kyampowell@gmail.com>
2020-11-23 12:40:55 -0700

    started lab 3
    adding player movement- up and down like flappy bird

---
+++ Lab 2 - Personal Project/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed;
+    private PlayerController playerControllerScript;
+    private float leftBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if game not over move left
+       if (! playerControllerScript.gameOver)
+       {
+           transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+       } 
+
+       //if object goes offscreen destroy it
+       if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+       {
+           Destroy(gameObject);
+       }
+    }
+}
---
+++ Lab 2 - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when space is pressed go up
+        if(Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+    }
+}
