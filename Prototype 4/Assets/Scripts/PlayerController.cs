b48f46f70f4ef244d25701947d130a802b991730
Kya <kyampowell@gmail.com>
2020-12-08 12:41:25 -0700

    4.3
    step 6 ish- trying to fix errors

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,4 +24,33 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        } }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup){
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        } }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Powerup")){
+            hasPowerup = true;
+            Destroy (other.gameObject);
+            StartCoroutine (PowerupCountdownRoutine());
+        }  }
+
+        IEnumeratorPowerupCountdownRoutine () 
+        {
+            yield return new WaitForSeconds(7);
+            hasPowerup = false; 
+        }
 }
